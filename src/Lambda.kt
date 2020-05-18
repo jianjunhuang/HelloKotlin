84ebe08b234ca291bdab3d383729a04b5c53dedf
JianjunHuang <demon903732218@gmail.com>
2020-05-18 20:06:04 +0800

    add Lambda & override operator

---
+++ src/Lambda.kt
@@ -0,0 +1,21 @@
+data class PersonL(val age: Int, val name: String)
+
+lateinit var test: PersonL
+var test2: PersonL? = null
+
+fun main(args: Array<String>) {
+    test = PersonL(0, "1")
+    val p = ::PersonL
+    val t = p(0, "2")
+    println(::PersonL.name)
+    test2?.let {
+        println(it)
+    }
+    val list = listOf(1, 2, 3, 4, 5)
+    list.asSequence()
+            .map { it + 1 }
+            .filter { it % 2 == 0 }
+            .toList()
+}
+
+
